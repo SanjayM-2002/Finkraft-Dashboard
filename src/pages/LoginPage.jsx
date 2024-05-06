40b638b32b44a8b50a6005e9441364a1edccb3b3
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-05-06 17:49:36 +0530

    completed login page components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Heading from '../components/Heading';
+import SubHeading from '../components/SubHeading';
+import InputBox from '../components/InputBox';
+import PrimaryButton from '../components/PrimaryButton';
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const submitForm = () => {
+    console.log('formdata is: ', formData);
+  };
   return (
     <>
       <div className='bg-slate-300 h-screen flex justify-center'>
         <div className='flex flex-col justify-center'>
           <div className='bg-yellow-200 w-96 pt-2 pb-2 pl-2 pr-2 mt-2 mb-2 ml-2 mr-2 rounded-md flex flex-col justify-center'>
             <Heading label={'Login Page'} />
+            <SubHeading
+              label={'Enter following information to login to dashboard'}
+            />
+            <InputBox
+              label={'First Name'}
+              placeholder={'John'}
+              name={'firstName'}
+              onChange={handleInput}
+              value={formData.firstName}
+            />
+            <InputBox
+              label={'Last Name'}
+              placeholder={'Doe'}
+              name={'lastName'}
+              onChange={handleInput}
+              value={formData.lastName}
+            />
+            <InputBox
+              label={'Username'}
+              placeholder={'johndoe'}
+              name={'username'}
+              onChange={handleInput}
+              value={formData.username}
+            />
+            <InputBox
+              label={'Password'}
+              placeholder={'JohnDoe123'}
+              name={'password'}
+              onChange={handleInput}
+              value={formData.password}
+            />
+            <PrimaryButton label={'Login'} onClick={submitForm} />
           </div>
         </div>
       </div>
